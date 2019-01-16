e08f8a724f483767fd8315e3e05e953a7e252154
Mauro Capelli <mcapelli@nwnit.com>
2019-01-16 17:02:44 -0500

    first broken test

--- tests/test_collector_kickstarts.py
+++ tests/test_collector_kickstarts.py
@@ -10,3 +10,10 @@ def test_main():
 
     assert result.output == '()\n'
     assert result.exit_code == 0
+
+
+def test_compare_files():
+    #  set file1 to some cretaed files
+    #  set file2  to simple_kickstart.good.txt
+
+    assert file1 == file2

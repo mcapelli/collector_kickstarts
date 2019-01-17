56292efbd41fad3f0ccc8b530639a681c94e4f0d
Mauro Capelli <mcapelli@nwnit.com>
2019-01-16 23:41:41 -0500

    math operations

--- tests/test_collector_kickstarts.py
+++ tests/test_collector_kickstarts.py
@@ -11,11 +11,12 @@ def test_main():
     assert result.output == '()\n'
     assert result.exit_code == 0
 
-
 def test_compare_files():
     #  set file1 to simple_kickstart.bad.txt
     file1 = open("simple_kickstart.bad.txt", "w+")
     #  set file2 to simple_kickstart.good.txt
     file2 = open("simple_kickstart.good.txt", "w+")
-
-    assert file1 == file2
+    f = 'A'
+    assert f == 'A'
+    a = 3 + 5 * 8 / 2
+    assert a == 32
